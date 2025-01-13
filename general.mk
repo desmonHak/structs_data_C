82917074143c245b8cb2948d0d1a8d9db02a4286
desmonHak <61366533+desmonHak@users.noreply.github.com>
2025-01-13 03:07:44 +0100

    eliminando llamada inecesaria

--- general.mk
+++ general.mk
@@ -1,20 +1,17 @@
 include config.mk
 
-all: libcolors.a libdebug.a $(TARGET).a
-	ar -t $(TARGET).a
+all: generate_lib
 	$(MAKE) -C . -f $(MAKE_NAME) examples
 
-examples: libcolors.a libdebug.a $(TARGET).a
+generate_lib: libdebug.a $(TARGET).a
+	ar -t $(TARGET).a
+
+examples: generate_lib
 	$(CC) examples/code_array-list.c    		 $(CFLAGS_EXAMPLES)   -o examples/code_array-list.$(EXTENSION)
 	$(CC) examples/code_hash-table.c    		 $(CFLAGS_EXAMPLES)   -o examples/code_hash-table.$(EXTENSION)
 	$(CC) examples/code_matriz-list.c   		 $(CFLAGS_EXAMPLES)   -o examples/code_matriz-list.$(EXTENSION)
 	$(CC) examples/code_vector-list.c   		 $(CFLAGS_EXAMPLES)   -o examples/code_vector-list.$(EXTENSION)
 	$(CC) examples/code-array-list_vector-list.c $(CFLAGS_EXAMPLES)	-o examples/code-array-list_vector-list.$(EXTENSION)
-
-
-libcolors.a:
-	echo "generando librerias estatica... $@"
-	$(MAKE) -C ./$(PATH_COLORS) -f $(MAKE_NAME)
 
 libdebug.a:
 	echo "generando librerias estatica... $@"
